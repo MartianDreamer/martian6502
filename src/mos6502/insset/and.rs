2bc6f5e4a374b57b6059bbe99c4d0ff8a21dc4f1
MartianDreamer <nguyenxuansang9494@gmail.com>
2024-10-04 08:48:20 +0700

    and

--- src/mos6502/insset/and.rs
+++ src/mos6502/insset/and.rs
@@ -1,4 +1,12 @@
-use super::{InsAttr, Mos6502Ins, Mos6502};
+use crate::mos6502::{
+    address_mode::{
+        absolute, absolute_x, absolute_y, immediate, indirect_x, indirect_y, zero_page,
+        zero_page_x, AddressModeFn,
+    },
+    constant::{NEGATIVE_ON_MASK, ZERO_ON_MASK},
+};
+
+use super::{InsAttr, Mos6502, Mos6502Ins};
 
 pub struct AndImm {
     pub attr: InsAttr,
@@ -27,41 +35,53 @@ pub struct AndIndY {
 
 impl Mos6502Ins for AndImm {
     fn execute(&self, cpu: &mut Mos6502) {
-        todo!()
+        do_and(cpu, &self.attr, immediate);
     }
 }
 impl Mos6502Ins for AndZP {
     fn execute(&self, cpu: &mut Mos6502) {
-        todo!()
+        do_and(cpu, &self.attr, zero_page);
     }
 }
 impl Mos6502Ins for AndZPX {
     fn execute(&self, cpu: &mut Mos6502) {
-        todo!()
+        do_and(cpu, &self.attr, zero_page_x);
     }
 }
 impl Mos6502Ins for AndAbs {
     fn execute(&self, cpu: &mut Mos6502) {
-        todo!()
+        do_and(cpu, &self.attr, absolute);
     }
 }
 impl Mos6502Ins for AndAbsX {
     fn execute(&self, cpu: &mut Mos6502) {
-        todo!()
+        do_and(cpu, &self.attr, absolute_x);
     }
 }
 impl Mos6502Ins for AndAbsY {
     fn execute(&self, cpu: &mut Mos6502) {
-        todo!()
+        do_and(cpu, &self.attr, absolute_y);
     }
 }
 impl Mos6502Ins for AndIndX {
     fn execute(&self, cpu: &mut Mos6502) {
-        todo!()
+        do_and(cpu, &self.attr, indirect_x);
     }
 }
 impl Mos6502Ins for AndIndY {
     fn execute(&self, cpu: &mut Mos6502) {
-        todo!()
+        do_and(cpu, &self.attr, indirect_y);
     }
+}
+
+fn do_and(cpu: &mut Mos6502, attr: &InsAttr, address_mode_fn: AddressModeFn) {
+    let operand = address_mode_fn(cpu);
+    let result = cpu.ac & operand;
+    if result >> 7 == 0b1 {
+        cpu.sr |= NEGATIVE_ON_MASK;
+    } else if result == 0 {
+        cpu.sr |= ZERO_ON_MASK;
+    }
+    cpu.ac = result;
+    cpu.next_instruction(attr);
 }

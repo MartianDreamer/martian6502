10371a3fed1c62ba6eaeaaf42aebf7659d3d8e20
MartianDreamer <nguyenxuansang9494@gmail.com>
2024-10-08 16:50:08 +0700

    cmp

--- src/mos6502/insset/cmp.rs
+++ src/mos6502/insset/cmp.rs
@@ -1,3 +1,11 @@
+use crate::mos6502::{
+    address_mode::{
+        absolute, absolute_x, absolute_y, immediate, indirect_x, indirect_y, zero_page,
+        zero_page_x, AddressModeFn,
+    },
+    constant::{CARRY_ON_MASK, NEGATIVE_ON_MASK, ZERO_ON_MASK},
+};
+
 use super::{InsAttr, Mos6502, Mos6502Ins};
 
 pub struct CmpImm {
@@ -34,48 +42,61 @@ pub struct CmpIndY {
 
 impl Mos6502Ins for CmpImm {
     fn execute(&self, cpu: &mut Mos6502) {
-        todo!()
+        do_compare(cpu, &self.attr, immediate);
     }
 }
 
 impl Mos6502Ins for CmpZP {
     fn execute(&self, cpu: &mut Mos6502) {
-        todo!()
+        do_compare(cpu, &self.attr, zero_page);
     }
 }
 
 impl Mos6502Ins for CmpZPX {
     fn execute(&self, cpu: &mut Mos6502) {
-        todo!()
+        do_compare(cpu, &self.attr, zero_page_x);
     }
 }
 
 impl Mos6502Ins for CmpAbs {
     fn execute(&self, cpu: &mut Mos6502) {
-        todo!()
+        do_compare(cpu, &self.attr, absolute);
     }
 }
 
 impl Mos6502Ins for CmpAbsX {
     fn execute(&self, cpu: &mut Mos6502) {
-        todo!()
+        do_compare(cpu, &self.attr, absolute_x);
     }
 }
 
 impl Mos6502Ins for CmpAbsY {
     fn execute(&self, cpu: &mut Mos6502) {
-        todo!()
+        do_compare(cpu, &self.attr, absolute_y);
     }
 }
 
 impl Mos6502Ins for CmpIndX {
     fn execute(&self, cpu: &mut Mos6502) {
-        todo!()
+        do_compare(cpu, &self.attr, indirect_x);
     }
 }
 
 impl Mos6502Ins for CmpIndY {
     fn execute(&self, cpu: &mut Mos6502) {
-        todo!()
+        do_compare(cpu, &self.attr, indirect_y);
     }
+}
+
+fn do_compare(cpu: &mut Mos6502, attr: &InsAttr, address_fn: AddressModeFn) {
+    let operand: u8 = address_fn(cpu);
+    if cpu.ac >= operand {
+        cpu.sr |= CARRY_ON_MASK
+    } else {
+        cpu.sr |= NEGATIVE_ON_MASK
+    }
+    if cpu.ac == operand {
+        cpu.sr |= ZERO_ON_MASK
+    }
+    cpu.next_instruction(attr);
 }

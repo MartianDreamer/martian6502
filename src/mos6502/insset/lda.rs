dc88634b15383d1c33787755de9022c5638d2dcc
MartianDreamer <nguyenxuansang9494@gmail.com>
2024-10-10 16:44:48 +0700

    lda

--- src/mos6502/insset/lda.rs
+++ src/mos6502/insset/lda.rs
@@ -1,4 +1,9 @@
-use super::{InsAttr, Mos6502, Mos6502Ins};
+use crate::mos6502::address_mode::{ absolute, absolute_x, absolute_y, immediate, indirect_x, indirect_y, zero_page, zero_page_x, AddressModeFn};
+
+use super::{
+    utils::{update_negative_flag, update_zero_flag},
+    InsAttr, Mos6502, Mos6502Ins,
+};
 
 pub struct LdaImm {
     pub attr: InsAttr,
@@ -34,48 +39,58 @@ pub struct LdaIndY {
 
 impl Mos6502Ins for LdaImm {
     fn execute(&self, cpu: &mut Mos6502) {
-        todo!()
+        do_load_accumulator(cpu, &self.attr, immediate);
     }
 }
 
 impl Mos6502Ins for LdaZP {
     fn execute(&self, cpu: &mut Mos6502) {
-        todo!()
+        do_load_accumulator(cpu, &self.attr, zero_page);
     }
 }
 
 impl Mos6502Ins for LdaZPX {
     fn execute(&self, cpu: &mut Mos6502) {
-        todo!()
+        do_load_accumulator(cpu, &self.attr, zero_page_x);
     }
 }
 
 impl Mos6502Ins for LdaAbs {
     fn execute(&self, cpu: &mut Mos6502) {
-        todo!()
+        do_load_accumulator(cpu, &self.attr, absolute);
     }
 }
 
 impl Mos6502Ins for LdaAbsX {
     fn execute(&self, cpu: &mut Mos6502) {
-        todo!()
+        do_load_accumulator(cpu, &self.attr, absolute_x);
     }
 }
 
 impl Mos6502Ins for LdaAbsY {
     fn execute(&self, cpu: &mut Mos6502) {
-        todo!()
+        do_load_accumulator(cpu, &self.attr, absolute_y);
     }
 }
 
 impl Mos6502Ins for LdaIndX {
     fn execute(&self, cpu: &mut Mos6502) {
-        todo!()
+        do_load_accumulator(cpu, &self.attr, indirect_x);
     }
 }
 
 impl Mos6502Ins for LdaIndY {
     fn execute(&self, cpu: &mut Mos6502) {
-        todo!()
+        do_load_accumulator(cpu, &self.attr, indirect_y);
     }
+}
+
+fn do_load_accumulator(cpu: &mut Mos6502, attr: &InsAttr, address_mode_fn: AddressModeFn) {
+    let mem_val: u8 = address_mode_fn(&cpu);
+    cpu.ac = mem_val;
+
+    update_zero_flag(cpu, cpu.ac == 0);
+    update_negative_flag(cpu, (cpu.ac as i8) < 0);
+
+    cpu.next_instruction(attr);
 }
